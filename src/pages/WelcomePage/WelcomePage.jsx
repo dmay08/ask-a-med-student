37016a1f2c22e25cfb1d33ce321d7cdbee4566fd
danny mayasich <dannymayasich@dannys-MacBook-Pro.local>
2018-11-28 13:13:37 -0800

    basic welcome page complete

---
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './WelcomePage.css';
+
+const Welcome = (props) => (
+    <div className="Welcome">
+        <h1>AMS</h1>
+        <p>Ask a Med Student</p>
+        <a href="">How it works</a>
+        <br></br>
+        <a href="">Sign Up</a>
+        <br></br>
+        <a href="">Log In</a>
+        <br></br>
+    </div>
+)
+
+export default Welcome;
