1a2d481804ab6972ae3bd55aac327995f283df27
danny mayasich <dannymayasich@dannys-MBP.attlocal.net>
2018-12-04 20:45:03 -0800

    final fix - centered AMS navbar logo - ready for Heroku deployment

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,9 +6,15 @@ const NavBar = (props) => {
     return (
         <div className='NavBar-wrap'>
             <div className='NavBar'>
-                <span className='NavBar-welcome'>{props.user.name}</span>
-                <h4>AMS</h4>
-                <Link to='/' onClick={props.handleLogout} className='NavBar-link' >Log Out</Link>
+                <div className="nav-thirds">
+                    <span className='NavBar-welcome'>{props.user.name}</span>
+                </div>
+                <div className="nav-thirds" id="second">
+                    <p>AMS</p>
+                </div>
+                <div className="nav-thirds" id="third">
+                    <Link to='/' onClick={props.handleLogout} className='NavBar-link' >Log Out</Link>
+                </div>
             </div>
         </div>
     );
